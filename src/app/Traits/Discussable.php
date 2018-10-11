90c03b100f2b9ccee99e609054811ec7a45d59e5
mishu <mishu@xtelecom.ro>
2018-10-11 08:28:00 +0300

    adds `onDelete` config option / improves `Discussable` trait

--- src/app/Traits/Discussable.php
+++ src/app/Traits/Discussable.php
@@ -3,9 +3,28 @@
 namespace LaravelEnso\Discussions\app\Traits;
 
 use LaravelEnso\CommentsManager\app\Models\Discussion;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 trait Discussable
 {
+    public static function bootDiscussable()
+    {
+        self::deleting(function ($model) {
+            if (config('enso.discussions.onDelete') === 'restrict'
+                && $model->discussions()->first() !== null) {
+                throw new ConflictHttpException(
+                    __('The entity has discussions and cannot be deleted')
+                );
+            }
+        });
+
+        self::deleted(function ($model) {
+            if (config('enso.discussions.onDelete') === 'cascade') {
+                $model->discussions()->delete();
+            }
+        });
+    }
+
     public function discussions()
     {
         return $this->morphMany(Discussion::class, 'discussable');
--- src/config/discussions.php
+++ src/config/discussions.php
@@ -3,6 +3,7 @@
 use LaravelEnso\Core\app\Models\UserGroup;
 
 return [
+    'onDelete' => 'restrict',
     'loggableMorph' => [
         'discussable' => [
             UserGroup::class => 'name',
