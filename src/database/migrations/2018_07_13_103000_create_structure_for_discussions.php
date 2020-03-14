b4c2b14476dc90bd0bf186ff9faed48b363f7d05
mishu <mishu@xtelecom.ro>
2020-03-14 18:26:29 +0200

    removes Types from migration

--- src/database/migrations/2018_07_13_103000_create_structure_for_discussions.php
+++ src/database/migrations/2018_07_13_103000_create_structure_for_discussions.php
@@ -1,19 +1,18 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForDiscussions extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.discussions.index', 'description' => 'Show index for discussion', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.discussions.store', 'description' => 'Store a new discussion', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.discussions.show', 'description' => 'Show discussion', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.discussions.update', 'description' => 'Update discussion', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.discussions.destroy', 'description' => 'Delete discussion', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.discussions.storeReply', 'description' => 'Store a new reply', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.discussions.updateReply', 'description' => 'Update reply', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.discussions.destroyReply', 'description' => 'Delete reply', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.discussions.react', 'description' => 'React on reactable', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.discussions.index', 'description' => 'Show index for discussion', 'is_default' => true],
+        ['name' => 'core.discussions.store', 'description' => 'Store a new discussion', 'is_default' => true],
+        ['name' => 'core.discussions.show', 'description' => 'Show discussion', 'is_default' => true],
+        ['name' => 'core.discussions.update', 'description' => 'Update discussion', 'is_default' => true],
+        ['name' => 'core.discussions.destroy', 'description' => 'Delete discussion', 'is_default' => true],
+        ['name' => 'core.discussions.storeReply', 'description' => 'Store a new reply', 'is_default' => true],
+        ['name' => 'core.discussions.updateReply', 'description' => 'Update reply', 'is_default' => true],
+        ['name' => 'core.discussions.destroyReply', 'description' => 'Delete reply', 'is_default' => true],
+        ['name' => 'core.discussions.react', 'description' => 'React on reactable', 'is_default' => true],
     ];
 }
