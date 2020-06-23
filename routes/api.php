f4d269e82553dc381b92416c0164bac0c413a636
reza <raftx24@gmail.com>
2020-06-23 19:30:04 +0430

    - changes `web` middleware to `api`

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth', 'core'])
+Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/core/discussions')->as('core.discussions.')
     ->namespace('LaravelEnso\Discussions\Http\Controllers')
     ->group(function () {
