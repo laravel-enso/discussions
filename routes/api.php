43ed64e3450aff80618bb301dd30d757a0cf7867
reza <raftx24@gmail.com>
2020-08-18 12:20:09 +0430

    adds xss-sanitizer

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['api', 'auth', 'core'])
+Route::middleware(['api', 'auth', 'core', 'xss-sanitizer:body'])
     ->prefix('api/core/discussions')->as('core.discussions.')
     ->namespace('LaravelEnso\Discussions\Http\Controllers')
     ->group(function () {
